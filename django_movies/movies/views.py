b6071809b99587148ee965dcc2f08476a7b82aa4
BarnetteME1 <BarnetteME1@gmail.com>
2015-10-24 18:45:05 -0400

    got movie titles on page

--- django_movies/movies/views.py
+++ django_movies/movies/views.py
@@ -1,7 +1,23 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from movies.models import Movie
+
 
 def index_view(request):
     return HttpResponse('hello')
+
+
+def movie_list_view(request):
+    movies = Movie.objects.all()
+    return render_to_response(template_name='movie_list.html', context={'movie_list': movies})
+
+
+class MovieList(ListView):
+    model = Movie
+
+
+class MovieDetail(DetailView):
+    model = Movie
